534087f98447de765bb0e45e07aeda50ff6bafcb
descent2oblivion <58195806+descent2oblivion@users.noreply.github.com>
2019-12-08 02:00:23 -0700

    Ya jalo :O

--- eva/app/src/main/java/com/example/eva/xper.kt
+++ eva/app/src/main/java/com/example/eva/xper.kt
@@ -3,15 +3,15 @@ package com.example.eva
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.xper.*
 import org.jetbrains.anko.alert
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
+import org.mozilla.javascript.Context
 import java.lang.Exception
-import javax.script.ScriptEngine
-import javax.script.ScriptEngineManager
 
 
 
@@ -22,18 +22,43 @@ class xper : AppCompatActivity() {
         setContentView(R.layout.xper)
     }
 
+    fun ran(view : View){
+
+        /*var engine = ScriptEngineManager().getEngineByName("rhino")
+        var Operation : String? = txtCode.getText().toString()!!
+
+        try {
+            doAsync {
+                var result = engine.eval(Operation, );
+                Log.d("Calculator", "Operation: " + Operation + " result: " + result);
+                uiThread {
+                    txtRes.setText(result.toString())
+                }
+            }
+        } catch (ex : Exception) {
+            Log.d("Calculator", " ScriptEngine error: " + ex.message);
+            txtRes.setText(ex.message)
+        }*/
+    }
+
     fun run(view : View){
 
+        var rhino = Context.enter()
+        // turn off optimization to work with android
+        rhino.optimizationLevel = -1
 
-        try{
-            with(ScriptEngineManager().getEngineByName("rhino")){
-                var z = eval("print('elo uorl')")
-                //txtRes.setText(z)
-            }
+        var evaluation = txtCode.getText().toString()
+
+        try {
+            var scope = rhino.initStandardObjects()
+            var result : String = rhino.evaluateString(scope, evaluation, "JavaScript", 1, null).toString()
+            txtRes.setText(result)
         }
-        catch (ex: Exception){
-            Toast.makeText(this, "nel", Toast.LENGTH_LONG).show()
-            txtRes.setText(ex.message.toString())
+        catch(e : Exception){
+            txtRes.setText(e.message)
+        }
+        finally {
+            Context.exit()
         }
     }
 }
